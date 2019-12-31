562c92dc15b6f4821a929653f97133f7df4909ca
David Taylor <david.taylor@auroraer.com>
2019-12-31 12:14:47 +0000

    Made a sad emoji spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject sadEmojiPrefab;
+    public float secondsBetweenSpawns = 1;
+    float nextSpawnTime;
+
+    Vector2 screenHalfSizeWorldUnits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenHalfSizeWorldUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawnTime)
+        {
+            nextSpawnTime = Time.time + secondsBetweenSpawns;
+            Vector2 spawnPosition = new Vector2(Random.Range(-screenHalfSizeWorldUnits.x, screenHalfSizeWorldUnits.x), screenHalfSizeWorldUnits.y);
+            Instantiate(sadEmojiPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
